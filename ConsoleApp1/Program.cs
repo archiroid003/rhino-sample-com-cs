e0ea8bcabc69f34d621bfc4eeda3b6b41f592e82
fujihira <fujihira@archiroid.com>
2018-10-27 20:09:51 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino4;
+using RhinoScript4;
+using Rhino.Geometry;
+
+namespace rhinoCOM
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            IRhino5Interface m_RhinoInterface = null;
+            RhinoScript rs = null;
+
+            Type type = Type.GetTypeFromProgID("Rhino5.Interface");
+            object obj = Activator.CreateInstance(type);
+            m_RhinoInterface = (IRhino5Interface)obj;
+
+            m_RhinoInterface.IsInitialized();
+            m_RhinoInterface.Visible = 1;
+
+            rs = m_RhinoInterface.GetScriptObject();
+
+            double[] pt_1 = { 0, 0, 0 };
+            double[] pt_2 = { 100, 100, 100 };
+
+            //rs.AddPoint(pt_1);
+
+            rs.AddLine(pt_1, pt_2);
+        }
+    }
+
+
+}
